30caae2bd8c715032f7348dd91d350fd14dc6f52
Gabriel Meireles <gmeireles@2mundos.net>
2021-01-29 18:42:14 -0300

    first commit

--- src/components/ui/Navigation/index.js
+++ src/components/ui/Navigation/index.js
@@ -6,7 +6,6 @@ function Navigation(props) {
     return (
         <div className="Navigation">
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <a className="navbar-brand" href="#">Carousel</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
